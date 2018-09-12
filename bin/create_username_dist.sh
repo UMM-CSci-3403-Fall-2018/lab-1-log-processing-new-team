88776901a8fa7b7b72a15aae646d89a1854df483
Nick Plucker <pluck011@morris.umn.edu>
2018-09-12 18:33:06 -0500

    Finish create_username_dist.sh and add comments for readabiliy

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
 path=$1
+here=$(pwd) # Mark the project directory
 
 cd $path
 
-cat failed_login_data.txt | awk '{print $4;}' | sort | uniq -c |
-    awk '{print "data.addRow([\x27"$2"\x27, \x27"$1"\x27]);"}' > uniq_test.txt
+# Takes the failed login data and converts it to the addRow javascript line and puts it in a temp file
+cat ./*/failed_login_data.txt | awk '{print $4;}' | sort | uniq -c |
+    awk '{print "data.addRow([\x27"$2"\x27, "$1"]);"}' > temp_addRow.txt
+
+# Concatenates the header, footer and temp file
+$here/bin/wrap_contents.sh temp_addRow.txt $here/html_components/username_dist username_dist.html
+
+# Removes temp file 
+rm temp_addRow.txt
+
+
--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -4,8 +4,7 @@ path=$1
 
 cd $path
 
-touch failed_login_data.txt
-
+# Finds the lines that have the failed password. Depending on what the failed password line contains, it's going to filter out the noise and put it in a failed_login_data file.  
 grep -hr "Failed password for" * | awk '{
 if ($9 =="invalid")
    print $1,$2,substr($3,0,2),$11,$13;
