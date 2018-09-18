a5abb16f597d940da9069b145507f6875e129449
Nick Plucker <pluck011@morris.umn.edu>
2018-09-18 10:50:42 -0500

    Finish process_logs.sh

--- bin/process_logs.sh
+++ bin/process_logs.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
+# Capture locations of places
 here=$(pwd)
 temp_dir=$(mktemp -d)
 
+cd $temp_dir
+
+# Take the input arguments and unzip them in the temp directory 
 for input_args in "$@"
 do
-    tar -xzf $input_args
-    archive_name=$(basename $input_args ".tgz")
-    ./bin/process_client_logs.sh $temp_dir/$archive_name
+	archive_name="$(basename $input_args '.tgz')"
+	mkdir $archive_name
+	tar -xzf $here/$input_args -C $archive_name
+       	$here/bin/process_client_logs.sh $archive_name
 done
 
+cd $here
+
+# Run the scripts to create the failed login summary report
 ./bin/create_username_dist.sh $temp_dir
 ./bin/create_hours_dist.sh $temp_dir
 ./bin/create_country_dist.sh $temp_dir
-./bin/assemble_reports.sh $temp_dir
+./bin/assemble_report.sh $temp_dir
 
+# Move the report from the temp directory to the project directory
 mv $temp_dir/failed_login_summary.html $here
 
+# Remove temp directory 
 rm -rf $temp_dir
     
